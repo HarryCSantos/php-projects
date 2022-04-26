2a66fd4092c72a47963370c4bbdee24f92186075
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-26 20:12:52 -0300

    alt

--- operacoes.php
+++ operacoes.php
@@ -3,4 +3,11 @@
 $idade = 32;
 $idadeDaqui10anos = $idade + 10; 
 
+$soma = 2 + 2;
+$subtracao = 2- 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
 echo $idadeDaqui10anos;
+
+echo $soma; $subtracao; $multiplicacao; $divisao;
