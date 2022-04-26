7687063e75adbd282099ad1329214d86356a3026
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-26 20:02:45 -0300

    alt

--- variavel.php
+++ variavel.php
@@ -2,4 +2,6 @@
 
 $idade = 32; // Para definir váriaveis no PHP se faz uso do $ antes da váriavel
 
+$idade = 33; // Sempre será válido o último valor atribuido a variável
+
 echo $idade; // echo é o comando utilizado para imprimir a linha é equivalente ao WriteLine do C#
