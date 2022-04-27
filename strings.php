929861bc0d4d68937d96c6192f645c5be60421fa
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-27 20:55:44 -0300

    alt

--- strings.php
+++ strings.php
@@ -3,5 +3,9 @@
 $idade = 32;
 
 echo "Olá mundo!\n"; // Contra barra n para quebrar linha
+
 echo "\t"; // Contra barra t para inseria um tab na string
-echo  "Minha idade é $idade  anos";
+
+echo  "Minha idade é $idade  anos" . PHP_EOL;
+
+echo "Harry";
