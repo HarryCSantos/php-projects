63f6fbdcbbc4a61d3cd81d0448ca2f1027983185
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-27 20:57:04 -0300

    alt

--- strings.php
+++ strings.php
@@ -6,6 +6,6 @@ echo "Olá mundo!\n"; // Contra barra n para quebrar linha
 
 echo "\t"; // Contra barra t para inseria um tab na string
 
-echo  "Minha idade é $idade  anos" . PHP_EOL;
+echo  "Minha idade é $idade  anos" . PHP_EOL; // PHP_EOL = END OF LINE TAMBÉM EXECUTA A QUEBRA DE LINHA ASSIN COMO \n
 
 echo "Harry";
