ea33e4f3ae8fb8fcf7046ab7baf833366b1a5e8f
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-27 20:50:04 -0300

    alt

--- strings.php
+++ strings.php
@@ -2,6 +2,6 @@
 
 $idade = 32;
 
-echo "Olá mundo!\n";
-echo "\t";
+echo "Olá mundo!\n"; // Contra barra n para quebrar linha
+echo "\t"; // Contra barra t para inseria um tab na string
 echo  "Minha idade é $idade  anos";
