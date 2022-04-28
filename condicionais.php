9aa1fec45167b51ea02eefc408788a73c8e31e1d
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-27 21:53:14 -0300

    alt

--- condicionais.php
+++ condicionais.php
@@ -1,16 +1,15 @@
 <?php
 
-$idade = 16;
+$idade = 34;
+$numeroDePessoas = 1;
 
-
-echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
+echo "Você só pode entrar se tiver a partir de 18 anos ou "; 
+echo "a partir de 16 anos acompanhado" .PHP_EOL;
 
 if ($idade >= 18) {
-    echo "Você tem $idade anos." .PHP_EOL;
-    echo "Pode entrar!";
+    echo "Você tem $idade anos . Pode entrar sozinho.";
 }
 
 else {
-    echo "Infelizmente você só tem  $idade anos" . PHP_EOL;
-    echo "Portanto não pode entrar!";
+    echo "Infelizmente você só tem  $idade anos. Você não pode entrar.";
 }
