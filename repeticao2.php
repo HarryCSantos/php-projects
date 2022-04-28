5c54bb190b7e4ebdec838c7ea10e4c4ae0415586
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-28 20:42:45 -0300

    alt

--- repeticao2.php
+++ repeticao2.php
@@ -2,8 +2,7 @@
 
 for ($contador = 1; $contador <= 15; $contador += 1){
     if ($contador != 13) { // != significa diferente
-        continue;
+        
+        echo "$contador" . PHP_EOL;
     }
-
-    echo "$contador" . PHP_EOL;
 }
