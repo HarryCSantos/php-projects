c5b279f5172b515de12b10b9325aff1c0b4e1206
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-28 20:59:08 -0300

    alt

--- desafios/imparate100.php
+++ desafios/imparate100.php
@@ -1,7 +1,7 @@
 <?php
 
-for ($contador = 1; $contador <=100; $contador ++){
-    if ($contador % 2 != 0) {
+for ($contador = 2; $contador <100; $contador ++){
+    if ($contador % 2 = = 0) {
         echo "$contador" . PHP_EOL;
     }
 }
