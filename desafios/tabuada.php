47685c9e5de189998d0082a4d0a97705bc8cfe97
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-28 21:19:46 -0300

    alt

--- desafios/tabuada.php
+++ desafios/tabuada.php
@@ -3,5 +3,5 @@
 $multiplicador = 3;
 
 for ($i = 1; $i <= 10; $i++){
-    echo "$multiplicador x $i = " . $multiplicador * $i . PHO_EOL;
+    echo "$multiplicador x $i = " . $multiplicador * $i . PHP_EOL;
 }
