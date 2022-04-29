19dd698955e355a65a138daa7f5d48f256dd7525
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-04-28 21:26:36 -0300

    alt

---
+++ desafios/imc.php
@@ -0,0 +1,17 @@
+<?php
+
+$peso = 74;
+$altura = 1.80;
+$imc = $peso / $altura **2;
+
+echo "Seu IMC é de $imc. Você está com o IMC ";
+
+if ($imc < 10) {
+    echo "abaixo";
+} elseif ($imc < 25) {
+    echo "dentro";
+} else {
+    echo "acima";
+}
+
+echo "do recomendado";
