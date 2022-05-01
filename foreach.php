495d84237040962902bfe05b484148140993e40c
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-01 06:18:58 -0300

    alt

--- foreach.php
+++ foreach.php
@@ -1,11 +1,8 @@
 <?php
 
-$conta1 = ['titular' => 'Harry', 'saldo' => 1000];
-$conta2 = ['titular' => 'Miguel', 'saldo' => 10000];
-$conta3 = ['titular' => 'Maria', 'saldo' => 5000];
+$contaCorrente = [12345678910 => ['titular' => 'Harry', 'saldo' => 1000], 12345678911 => ['titular' => 'Miguel', 'saldo' => 10000], 12345678912 => ['titular' => 'Maria', 'saldo' => 5000]
+];
 
-$contaCorrente = [$conta1, $conta2, $conta3];
-
-foreach ($contaCorrente as $conta) {
-    echo $conta['titular'] .PHP_EOL;
+foreach ($contaCorrente as $cpf => $conta) {
+    echo $cpf .PHP_EOL;
 }
--- listas.php
+++ listas.php
@@ -1,6 +1,9 @@
 <?php
 
 $idadeList = [22, 12, 15, 43, 53, 5];
-$age = $idadeList [3];
 
-echo $idadeList;
+$idadeList[count($idadeList)] = 20;
+
+foreach ($idadeList as $idade) {
+    echo $idade .PHP_EOL;
+}
