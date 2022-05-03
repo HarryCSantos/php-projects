6ca2cf158a08509985fe6e2c0da3eefdbfeb795e
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-03 03:29:38 -0300

    alt

--- banco.php
+++ banco.php
@@ -1,5 +1,16 @@
 <?php
 
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo'])
+    {
+        exibeMensagem("Você não pode sacar este valor");
+    }
+    else{
+        $conta['saldo'] -= $valorASacar;
+    }
+}
+
 $contaCorrente = [12345678910 => ['titular' => 'Harry',
 'saldo' => 1000], 12345678911 => ['titular' => 'Miguel',
 'saldo' => 10000], 12345678912 => ['titular' => 'Maria', 'saldo' => 5000]
