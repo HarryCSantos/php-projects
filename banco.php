cfe24009ce4e31bc8278d24433642d94799e2252
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-03 02:56:06 -0300

    alt

--- banco.php
+++ banco.php
@@ -5,6 +5,8 @@ $contaCorrente = [12345678910 => ['titular' => 'Harry',
 'saldo' => 10000], 12345678912 => ['titular' => 'Maria', 'saldo' => 5000]
 ];
 
+$contaCorrente[12345678910]['saldo'] -= 500;
+
 foreach ($contaCorrente as $cpf => $conta) {
     echo $cpf . " " . $conta['titular'] .PHP_EOL;
 }
