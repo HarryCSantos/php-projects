d302939be1aa30b1b8f87e182b53a64cbaa6c9ec
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-06 03:23:01 -0300

    alt

--- funcoes.php
+++ funcoes.php
@@ -30,6 +30,11 @@ function depositar($conta, $valorADepositar)
     return $conta;
 }
 
+$contaCorrente = [12345678910 => ['titular' => 'Harry',
+'saldo' => 1000], 12345678911 => ['titular' => 'Miguel',
+'saldo' => 10000], 12345678912 => ['titular' => 'Maria', 'saldo' => 5000]
+];
+
 foreach ($contaCorrente as $cpf => $conta) {
     exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
 }
