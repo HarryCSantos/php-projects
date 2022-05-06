3a2475330baa8c778b2d62727d596701e2cc501a
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-06 02:38:56 -0300

    alt

--- banco.php
+++ banco.php
@@ -15,7 +15,6 @@ function sacar($conta, $valorASacar)
     return $conta;
 }
 
-
 $contaCorrente = [12345678910 => ['titular' => 'Harry',
 'saldo' => 1000], 12345678911 => ['titular' => 'Miguel',
 'saldo' => 10000], 12345678912 => ['titular' => 'Maria', 'saldo' => 5000]
