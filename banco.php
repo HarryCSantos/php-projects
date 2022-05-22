37e8b2909f42e66ae7e65ec191ff08489e74783d
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-22 18:41:52 -0300

    alt

--- banco.php
+++ banco.php
@@ -27,7 +27,7 @@ echo "</ul>";
         <title>Document</title>
     </head>
 <body>
-    <h1>Contas Correntes access unit</h1>
+    <h1>Contas Correntes access unit sequence</h1>
     <dl>
         <?php foreach($contaCorrente as $cpf => $conta)
         { ?>
