dc5bf1938a22064bb4469cca1efd230655a25854
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-05-26 16:41:58 -0300

    alt

--- banco.php
+++ banco.php
@@ -27,7 +27,7 @@ echo "</ul>";
         <title>Document</title>
     </head>
 <body>
-    <h1>Contas Correntes access unit sequence duolingo familia DEUS</h1>
+    <h1>Contas Correntes access unit sequence duolingo familia DEUS e o caminho</h1>
     <dl>
         <?php foreach($contaCorrente as $cpf => $conta)
         { ?>
